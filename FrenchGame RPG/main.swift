974b875e0c939d2b398133462c76b602e79bd081
Oscar RENIER <oscar.renier@epitech.eu>
2019-07-10 00:04:53 +0200

    Initial Commit

---
+++ FrenchGame RPG/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  FrenchGame RPG
+//
+//  Created by Oscar RENIER on 10/07/2019.
+//  Copyright © 2019 Oscar RENIER. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
